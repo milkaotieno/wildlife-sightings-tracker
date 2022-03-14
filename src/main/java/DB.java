6b6c9d56889a65bde3bdf6f0352759276d3400e6
milkaotieno <milka.otieno@student.moringaschool.com>
2022-03-15 02:57:25 +0300

    final commit

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,7 +1,9 @@
-public class DB {
-    public static String connectionString = "jdbc:postgresql://ec2-44-194-167-63.compute-1.amazonaws.com:5432/df00c1fkrh14f6";
+import org.sql2o.Sql2o;
 
-    //Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "moringa", "Access");
-//    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-3-92-119-83.compute-1.amazonaws.com:5432/d7old5hsbjkqm4", "qkobqpnyotbkek","69228f5000c01877893dc747578f59f25ba53696b3de5fef398a82f0a0cfae71" ); //!
+public class DB {
+   // public static Sql2o sql2o = new Sql2o ("jdbc:postgresql://ec2-44-194-167-63.compute-1.amazonaws.com:5432/df00c1fkrh14f6");
+
+    //Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "moringa", "Access");//
+  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-44-194-167-63.compute-1.amazonaws.com:5432/df00c1fkrh14f6", "nimjoyoxqqntae","aec6525ee5a740eca400d801b5976a3cc6a2f52727218b1df65024bfa1199f45" );
 
 }
